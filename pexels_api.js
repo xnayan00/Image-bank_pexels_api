dac38f401f2c40e36d9627fca09c6b35439e63e6
xnayn00 <yan_mendes.18@outlook.com>
2021-10-14 18:29:18 -0300

    GET method encapsulation

--- pexels_api.js
+++ pexels_api.js
@@ -1,21 +1,43 @@
-var api = new XMLHttpRequest()
-var imgCardContent = document.getElementById('imgCardContent')
+const api = new XMLHttpRequest()
+const imgCardContent = document.getElementById('imgCardContent')
+var photosObj = []
+var nextPage = ''
+var prevPage = ''
 
-/*****   Pexels API GET photos   *****/
-api.open('GET', 'https://api.pexels.com/v1/search?query=office&per_page=16')
-api.setRequestHeader(
-    "Authorization",
-    "563492ad6f917000010000018bf00f1cdc874994bd214ddf5cdd6235"
-);
-api.onreadystatechange = function setPhotosObj() {
-    if(api.readyState === 4){        
-        let photosObj = JSON.parse(api.responseText)
+function getPhotos(value, page) {
+    imgCardContent.innerHTML = ""
 
-        /*****   insert images dynamically   *****/
-        photosObj.photos.map(photo => {
-            imgCardContent.innerHTML +=
-                `<div class="c-image-card"><img src="${photo.src.tiny}"></div>`
-        })
+    if(!value){
+        value = 'house'
     }
+
+    if(nextPage !== '' && page === 'next'){
+        api.open('GET', nextPage)
+    } else if(prevPage !== '' && page === 'prev'){
+        api.open('GET', prevPage)
+    } else {
+        api.open('GET', `https://api.pexels.com/v1/search?query=${value}`)
+    }
+    api.setRequestHeader(
+        "Authorization",
+        "563492ad6f917000010000018bf00f1cdc874994bd214ddf5cdd6235"
+    )
+    api.onreadystatechange = () => {
+        if(api.readyState === 4){ // verify if the request state is equal 4 (done)
+            photosObj = JSON.parse(api.responseText)
+            if(photosObj.page > 1){
+                nextPage = photosObj.next_page
+                prevPage = photosObj.prev_page
+            } else {
+                nextPage = photosObj.next_page
+            }
+            /*****   insert images dynamically   *****/
+            return photosObj.photos.map(photo => {
+                imgCardContent.innerHTML +=
+                    `<div class="c-gallery-image"><img src="${photo.src.tiny}"></div>`
+            })
+        }
+    }
+    api.send() // send the request
 }
-api.send()
+getPhotos() // Initialize with some photos
