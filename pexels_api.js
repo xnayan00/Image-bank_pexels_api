8b8deea5ffc8b7d6c25c8d0634d1d88776001126
xnayn00 <yan_mendes.18@outlook.com>
2021-10-14 20:00:34 -0300

    Style of the next and prev buttons and creation of this interation

--- pexels_api.js
+++ pexels_api.js
@@ -1,8 +1,11 @@
 const api = new XMLHttpRequest()
 const imgCardContent = document.getElementById('imgCardContent')
+const nextButton = document.getElementById('nextButton')
+const prevButton = document.getElementById('prevButton')
 var photosObj = []
 var nextPage = ''
 var prevPage = ''
+var currentPage = 1
 
 function getPhotos(value, page) {
     imgCardContent.innerHTML = ""
@@ -16,7 +19,7 @@ function getPhotos(value, page) {
     } else if(prevPage !== '' && page === 'prev'){
         api.open('GET', prevPage)
     } else {
-        api.open('GET', `https://api.pexels.com/v1/search?query=${value}`)
+        api.open('GET', `https://api.pexels.com/v1/search?query=${value}&per_page=3`)
     }
     api.setRequestHeader(
         "Authorization",
@@ -25,6 +28,25 @@ function getPhotos(value, page) {
     api.onreadystatechange = () => {
         if(api.readyState === 4){ // verify if the request state is equal 4 (done)
             photosObj = JSON.parse(api.responseText)
+            currentPage = photosObj.page
+
+            if(photosObj.hasOwnProperty('prev_page')){
+                prevButton.classList.remove('c-button--disabled')
+                prevButton.removeAttribute('disabled', 'disabled')
+            }
+            if(photosObj.hasOwnProperty('prev_page')){
+                nextButton.classList.remove('c-button--disabled')
+                nextButton.removeAttribute('disabled', 'disabled')
+            }
+            if(!photosObj.hasOwnProperty('next_page')) {
+                nextButton.classList.add('c-button--disabled')
+                nextButton.setAttribute('disabled', 'disabled')
+            }
+            if(!photosObj.hasOwnProperty('prev_page')){
+                prevButton.classList.add('c-button--disabled')
+                prevButton.setAttribute('disabled', 'disabled')
+            }
+
             if(photosObj.page > 1){
                 nextPage = photosObj.next_page
                 prevPage = photosObj.prev_page
@@ -40,4 +62,5 @@ function getPhotos(value, page) {
     }
     api.send() // send the request
 }
+
 getPhotos() // Initialize with some photos
